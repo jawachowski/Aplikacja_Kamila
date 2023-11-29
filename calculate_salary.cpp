9a951a7b6526ebc54d4ca97b4371bac1e17af5d6
Jakub Wachowski <131616032+jawachowski@users.noreply.github.com>
2023-11-29 08:34:27 +0100

    Update calculate_salary.cpp

--- calculate_salary.cpp
+++ calculate_salary.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+using namespace std;
 
 double calculate_salary(double sales_amount, double advance_amount) {
     double bonus = calculate_bonus(sales_amount);
     double total_salary = sales_amount * (1 + bonus) - advance_amount;
 
     if (total_salary < 0) {
-        std::cout << "Pracownik musi zwrócić firmie pieniądze." << std::endl;
+        cout << "Pracownik musi zwrócić firmie pieniądze." << std::endl;
     }
 
-    return std::max(total_salary, 0.0);
+    return max(total_salary, 0.0);
 }
